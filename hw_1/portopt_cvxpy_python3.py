e6c4f5f80e81c5ef21b597e3020b8ca5f4b0ea30
Oscar <ogcg12@gmail.com>
2021-08-30 00:33:14 -0500

    Finished homework 1

--- hw_1/portopt_cvxpy_python3.py
+++ hw_1/portopt_cvxpy_python3.py
@@ -13,88 +13,66 @@ Author: Shabbir Ahmed
 import pandas as pd
 import numpy as np
 from cvxpy import *
+from base import ROOT_DIR
 
 # read monthly_prices.csv
-mp = pd.read_csv("data/monthly_prices.csv", index_col=0)
-mr = pd.DataFrame()
+mp = pd.read_csv(ROOT_DIR / "hw_1/data/monthly_prices.csv", index_col=0)
 
-# compute monthly returns
-for s in mp.columns:
-    date = mp.index[0]
-    pr0 = mp[s][date] 
-    for t in range(1,len(mp.index)):
-        date = mp.index[t]
-        pr1 = mp[s][date]
-        ret = (pr1-pr0)/pr0
-        #mr.set_value(date,s,ret)
-        mr.at[date,s]=ret
-        pr0 = pr1
-        
-# get symbol names
-symbols = mr.columns
 
-# convert monthly return data frame to a numpy matrix
-#return_data = mr.as_matrix().T
-return_data = mr.values.T
+def opt_portfolio(mp: pd.DataFrame):
+    mr = pd.DataFrame()
 
-# compute mean return
-r = np.asarray(np.mean(return_data, axis=1))
+    # compute monthly returns
+    for s in mp.columns:
+        date = mp.index[0]
+        pr0 = mp[s][date]
+        for t in range(1, len(mp.index)):
+            date = mp.index[t]
+            pr1 = mp[s][date]
+            ret = (pr1 - pr0) / pr0
+            # mr.set_value(date,s,ret)
+            mr.at[date, s] = ret
+            pr0 = pr1
 
-# covariance
-C = np.asmatrix(np.cov(return_data))
+    # get symbol names
+    symbols = mr.columns
 
-# print out expected return and std deviation
-print("----------------------")
-for j in range(len(symbols)):
-    print('%s: Exp ret = %f, Risk = %f' %(symbols[j],r[j], C[j,j]**0.5))
-   
+    # convert monthly return data frame to a numpy matrix
+    # return_data = mr.as_matrix().T
+    return_data = mr.values.T
 
-# set up optimization model
-n = len(symbols)
-x = Variable(n)
-req_return = 0.02
-ret = r.T*x
-risk = quad_form(x, C)
-prob = Problem(Minimize(risk), 
-               [sum(x) == 1, ret >= req_return, x >= 0])
+    # compute mean return
+    r = np.asarray(np.mean(return_data, axis=1))
 
-# solve problem and write solution
-try:
-    prob.solve()
+    # covariance
+    C = np.asmatrix(np.cov(return_data))
+
+    # print out expected return and std deviation
     print("----------------------")
-    print("Optimal portfolio")
-    print("----------------------")
-    for s in range(len(symbols)):
-        #print('x[%s] = %f'%(symbols[s],x.value[s,0]))
-        print('x[%s] = %f'%(symbols[s],x.value[s]))
-    print("----------------------")
-    print('Exp ret = %f' %(ret.value))
-    print('risk    = %f' %((risk.value)**0.5))
-    print("----------------------")
-except:
-    print('Error')
+    for j in range(len(symbols)):
+        print('%s: Exp ret = %f, Risk = %f' % (symbols[j], r[j], C[j, j] ** 0.5))
 
+    # set up optimization model
+    n = len(symbols)
+    x = Variable(n)
+    req_return = 0.02
+    ret = r.T @ x
+    risk = quad_form(x, C)
+    prob = Problem(Minimize(risk),
+                   [sum(x) == 1, ret >= req_return, x >= 0])
 
-# In[7]:
-
-
-symbols
-
-
-# In[8]:
-
-
-x[0]
-
-
-# In[10]:
-
-
-x.value[0]
-
-
-# In[ ]:
-
-
-
-
+    # solve problem and write solution
+    try:
+        prob.solve()
+        print("----------------------")
+        print("Optimal portfolio")
+        print("----------------------")
+        for s in range(len(symbols)):
+            # print('x[%s] = %f'%(symbols[s],x.value[s,0]))
+            print('x[%s] = %f' % (symbols[s], x.value[s]))
+        print("----------------------")
+        print('Exp ret = %f' % (ret.value))
+        print('risk    = %f' % ((risk.value) ** 0.5))
+        print("----------------------")
+    except:
+        print('Error')
